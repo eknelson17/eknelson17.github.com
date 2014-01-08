01da4619076cc1b4ec481b6a538a60e8cd575d2f
Emma Nelson <eknelson17@gmail.com>
2014-01-08 14:51:14 -0500

    Added content to coding projects page
    - Copied over and added to content for technical programming projects
    - Reformatted to fit theme
    - Added a lightbox plug-in for Bootstrap
      - Customized it slightly to use font-awesome
    - Added back in font files so they stop getting 404 errors
    
    TODO
    - Need separate thumbnail files, instead of shrinking images using CSS

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('li.active ul li').removeClass('active');
 
+  $('#blueimp-gallery').data('useBootstrapModal', false);
   // Modals
   // $('#daleks').modal();
   // $('#monkey').modal();
